f706ac27fe6399c460f92b2be8701673731b3f25
Alexander Gil Fuentes <ag3339@guest-dhc-www-dev1.cul.columbia.edu>
2015-09-19 16:18:50 -0400

    first commit

---
+++ themes/berlin/index.php
@@ -0,0 +1,63 @@
+<?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
+<div id="primary">
+	<a href="/neatline"><img src="/images/1891map.jpg" width="100%"/></a>
+
+</div><!-- end primary -->
+
+<div id="secondary">
+   <div id="sidebar" class="widget">
+  <ul>
+
+      <a class="title" data-id="6" href="/exhibits/show/bloomingdaleasylum">
+        Bloomingdale Asylum
+      </a>
+
+      <a class="title" data-id="3" href="/exhibits/show/mural">
+        Butler Library Mural
+      </a>
+
+      <a class="title" data-id="2" href="/exhibits/show/ccsjd">
+       St. John the Divine
+      </a>
+
+      <a class="title" data-id="7" href="/exhibits/show/engine_company_no_47">
+        Engine Company No. 47
+      </a>
+
+
+
+      <a class="title" data-id="9" href="/exhibits/show/irt">
+        Interborough Rapid Transit
+      </a>
+
+
+       <a class="title" data-id="11" href="/exhibits/show/leakeandwattsorphanhouse">
+        Leake & Watts Orphan House
+      </a>
+
+      <a class="title" data-id="4" href="/exhibits/show/lion-brewery-history/lion-brewery-details">
+        Lion Brewery
+      </a>
+<!--
+      <a class="title" data-id="4" href="/exhibits/show/lion-brewery-advertising">
+        Lion Brewery II
+      </a>
+ -->
+
+      <a class="title" data-id="5" href="/exhibits/show/the-riverside-church">
+        The Riverside Church
+      </a>
+
+      <a class="title" data-id="8" href="/exhibits/show/riverside-park">
+        Riverside Park
+      </a>
+
+      <a class="title" data-id="10" href="/exhibits/show/hospital">
+        St. Luke's Hospital
+      </a>
+
+  </ul>
+  </div>
+
+</div><!-- end secondary -->
+<?php echo foot(); ?>
